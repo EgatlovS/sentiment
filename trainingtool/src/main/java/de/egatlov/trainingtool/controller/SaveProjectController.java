217b4b5d6e32f0f9a307d7ff3a5596a4d98d2270
EgatlovS <alex.bogner36@googlemail.com>
2017-04-02 20:40:20 +0200

    Added SaveProjectControllerSkeleton and added SaveProjectView

---
+++ trainingtool/src/main/java/de/egatlov/trainingtool/controller/SaveProjectController.java
@@ -0,0 +1,38 @@
+package de.egatlov.trainingtool.controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class SaveProjectController {
+
+    @FXML
+    private JFXTextField saveToTF;
+
+    @FXML
+    private JFXButton browseBtn;
+
+    @FXML
+    private JFXButton saveBtn;
+
+    @FXML
+    private JFXButton cancelBtn;
+
+    @FXML
+    void browse(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void save(ActionEvent event) {
+
+    }
+
+}
