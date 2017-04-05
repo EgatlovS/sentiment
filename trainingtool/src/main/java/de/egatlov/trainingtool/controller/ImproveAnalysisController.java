a775b7aff22bec8c74c9b3468c7c390507627cad
EgatlovS <alex.bogner36@googlemail.com>
2017-04-05 21:57:59 +0200

    Added ImproveSentimentView and the controller skeleton

---
+++ trainingtool/src/main/java/de/egatlov/trainingtool/controller/ImproveAnalysisController.java
@@ -0,0 +1,30 @@
+package de.egatlov.trainingtool.controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class ImproveAnalysisController {
+
+	@FXML
+	private JFXComboBox<?> chooseSentimentCB;
+
+	@FXML
+	private JFXButton improveBtn;
+
+	@FXML
+	private JFXButton canceBtn;
+
+	@FXML
+	void cancel(ActionEvent event) {
+
+	}
+
+	@FXML
+	void improve(ActionEvent event) {
+
+	}
+
+}
